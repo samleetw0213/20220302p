bafc5ae945edf383f1cc6e37fdcd7a39d1aa94f3
samleetw0213 <100757333+samleetw0213@users.noreply.github.com>
2022-03-02 16:44:46 +0800

    init
    0302pinit

---
+++ src/test/java/com/myorg/HelloCdk3Test.java
@@ -0,0 +1,26 @@
+// package com.myorg;
+
+// import software.amazon.awscdk.App;
+// import software.amazon.awscdk.assertions.Template;
+// import java.io.IOException;
+
+// import java.util.HashMap;
+
+// import org.junit.jupiter.api.Test;
+
+// example test. To run these tests, uncomment this file, along with the
+// example resource in java/src/main/java/com/myorg/HelloCdk3Stack.java
+// public class HelloCdk3Test {
+
+//     @Test
+//     public void testStack() throws IOException {
+//         App app = new App();
+//         HelloCdk3Stack stack = new HelloCdk3Stack(app, "test");
+
+//         Template template = Template.fromStack(stack);
+
+//         template.hasResourceProperties("AWS::SQS::Queue", new HashMap<String, Number>() {{
+//           put("VisibilityTimeout", 300);
+//         }});
+//     }
+// }
