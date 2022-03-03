76716526264aecf501845b009fed9015ee4e4d2d
samleetw0213 <100757333+samleetw0213@users.noreply.github.com>
2022-03-03 08:52:59 +0800

    change stackname01

--- src/main/java/com/nv/HelloWork1.java
+++ src/main/java/com/nv/HelloWork1.java
@@ -9,7 +9,7 @@ public class HelloWork1 {
     public static void main(final String[] args) {
         App app = new App();
 
-        new HelloCdk3Stack(app, "HelloCdk3Stack", StackProps.builder()
+        new HelloCdk3Stack(app, "stackname01", StackProps.builder()
                 // If you don't specify 'env', this stack will be environment-agnostic.
                 // Account/Region-dependent features and context lookups will not work,
                 // but a single synthesized template can be deployed anywhere.
